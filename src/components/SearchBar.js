bb7f59dabeabadccaf0c697392f0504d6652ca5c
iNeedMyGolem <54441424+iNeedMyGolem@users.noreply.github.com>
2021-02-21 21:36:38 +0200

    add project

---
+++ src/components/SearchBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    state = { searchWord: '' };
+
+    onInputChange = (event) => {
+        this.setState({ searchWord: event.target.value });
+    };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        
+        this.props.onFormSubmit(this.state.searchWord)
+    };
+
+    render() {
+        return (
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input 
+                            type="text" 
+                            value={this.state.searchWord} 
+                            onChange={this.onInputChange} 
+                        />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
